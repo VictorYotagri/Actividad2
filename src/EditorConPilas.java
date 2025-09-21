06854879ed80eadc4ec105d39e4186ec48dca166
Chica31 <163798665+Chica31@users.noreply.github.com>
2025-09-21 18:15:25 -0500

    Primero EditorConPilas.java

--- src/EditorConPilas.java
+++ src/EditorConPilas.java
@@ -1,12 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-kjnnknknnk
+import java.util.Scanner;
+//  Actividad 2 Pilas ( Stack )
+class Pila {
+    private String[] elementos;  // Arreglo para guardar los elementos
+    private int tope;           // Índice del último elemento
+    private int tamaño;         // Tamaño máximo de la pila
 
-    kllllklkl
-    kmlmñkm
-
-    ´kñkñlk
-    klklk
-        }
+    // Se crea una pila vacia
+    public Pila(int tamaño) {
+        this.tamaño = tamaño;
+        this.elementos = new String[tamaño];
+        this.tope = -1;  // el -1 significa que está vacía
     }
-}
